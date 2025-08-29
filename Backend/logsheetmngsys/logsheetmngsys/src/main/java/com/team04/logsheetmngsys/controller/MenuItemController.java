c2a26e965934f092d7c80eb5387eb88fe4d31413
Ravindra Shende <ravindrashende7867@gmail.com>
2025-08-29 09:20:53 +0530

    menu_items endpoint completed

---
+++ Backend/logsheetmngsys/logsheetmngsys/src/main/java/com/team04/logsheetmngsys/controller/MenuItemController.java
@@ -0,0 +1,69 @@
+package com.team04.logsheetmngsys.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.team04.logsheetmngsys.dto.MenuItemDTO;
+import com.team04.logsheetmngsys.entity.MenuItem;
+import com.team04.logsheetmngsys.service.MenuItemService;
+
+@RestController
+@RequestMapping("/api/menu-items")
+public class MenuItemController {
+
+	private final MenuItemService menuItemService;
+
+	@Autowired
+	public MenuItemController(MenuItemService menuItemService) {
+		this.menuItemService = menuItemService;
+	}
+
+	@PostMapping
+	public ResponseEntity<MenuItem> createMenuItem(@RequestBody MenuItemDTO menuItemDTO) {
+		MenuItem createdMenuItem = menuItemService.createMenuItem(menuItemDTO);
+		return new ResponseEntity<>(createdMenuItem, HttpStatus.CREATED);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<MenuItem>> getAllMenuItems() {
+		List<MenuItem> menuItems = menuItemService.getAllMenuItems();
+		return new ResponseEntity<>(menuItems, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<MenuItem> getMenuItemById(@PathVariable Long id) {
+		MenuItem menuItem = menuItemService.getMenuItemById(id);
+		return new ResponseEntity<>(menuItem, HttpStatus.OK);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<MenuItem> updateMenuItem(@PathVariable Long id, @RequestBody MenuItemDTO menuItemDTO) {
+		MenuItem updatedMenuItem = menuItemService.updateMenuItem(id, menuItemDTO);
+		return new ResponseEntity<>(updatedMenuItem, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteMenuItem(@PathVariable Long id) {
+		try {
+			menuItemService.deleteMenuItem(id);
+			return new ResponseEntity<>("MenuItem deleted successfully", HttpStatus.OK);
+		} catch (ResponseStatusException e) {
+			return new ResponseEntity<>(e.getReason(), HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			return new ResponseEntity<>("An internal server error occurred during deletion.",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
