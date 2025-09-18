86da7512962a7fd1a8942b98d90370f583871b70
KD4-Suraj-83511 <surajskamble2001@gmail.com>
2025-09-18 10:39:35 +0530

    admin components added

---
+++ Frontend/logsheetmng/src/components/AdminNavbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import logo from "../assets/mainlogo.png";
+
+export default function AdminNavbar() {
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+
+  return (
+    <Navbar bg="light" expand="lg" fixed="top" className="shadow-sm admin-navbar">
+      <Container /* fluid if you want full width: fluid */>
+        <Navbar.Brand href="/admin" className="d-flex align-items-center">
+          <img src={logo} alt="Logo" width="40" height="40" className="me-2" />
+          <span className="fw-bold">LogiTrack Admin</span>
+        </Navbar.Brand>
+        <Nav className="ms-auto">
+          <Button variant="outline-danger" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+
